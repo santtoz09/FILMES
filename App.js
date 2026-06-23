2b9bfb89961d855f73cb646f07afa9310552ed95
Your Name <you@example.com>
2026-06-23 18:03:16 -0300

    adicionando o carrousel de filmes + json com caracteristicas de filmes.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput,FlatList } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
 import Header from './src/components/header';
 import Search from './src/components/search';
 import Banner from './src/components/banner';
-
+import Filmes from './data/movies';
 
 export default function App() {
   return (
@@ -14,6 +14,24 @@ export default function App() {
       <Search></Search>
       <Banner></Banner>
 
+
+      <View style={{width: '90%'}}>
+        <FlatList
+          horizontal= {true}
+          data= {Filmes}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => (
+            <TouchableOpacity>
+              <Image style={{width: '80%', height: '100%'}} source={{uri: item.imagem}}></Image>
+              <Text> {item.nome} </Text>
+            </TouchableOpacity>
+          )}
+        
+        
+        />
+
+
+      </View>
     </View>
   );
 }
---
+++ data/movies.js
@@ -0,0 +1,58 @@
+const DATA = [
+    {
+      id: 1,
+      nome: "The Wolf of Wall Street",
+      imagem: "https://s3.amazonaws.com/nightjarprod/content/uploads/sites/238/2023/08/23151410/pWHf4khOloNVfCxscsXFj3jj6gP-718x1024.jpg",
+      imdb: 8.2
+    },
+    {
+      id: 2,
+      nome: "Michael",
+      imagem: "https://m.media-amazon.com/images/S/pv-target-images/b4dddb47fec1709126a5542c1a0e09123ae0cd9ad47dfbe9ed23a222d2990d44._UR2000,3000_.jpg",
+      imdb: 5.7
+    },
+    {
+      id: 3,
+      nome: "Fast & Furious",
+      imagem: "https://upload.wikimedia.org/wikipedia/pt/8/8f/Fast_and_Furious_Poster.jpg",
+      imdb: 6.5
+    },
+    {
+      id: 4,
+      nome: "Get Rich or Die Tryin'",
+      imagem: "https://m.media-amazon.com/images/M/MV5BMTI4NTE0MTU2N15BMl5BanBnXkFtZTYwNzQ1OTc2._V1_.jpg",
+      imdb: 5.5
+    },
+    {
+      id: 5,
+      nome: "The Godfather",
+      imagem: "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
+      imdb: 9.2
+    },
+    {
+      id: 6,
+      nome: "Joker",
+      imagem: "https://upload.wikimedia.org/wikipedia/en/e/e1/Joker_%282019_film%29_poster.jpg",
+      imdb: 8.3
+    },
+    {
+      id: 7,
+      nome: "The Matrix",
+      imagem: "https://tse4.mm.bing.net/th/id/OIP.BcekSEbcVAUgayzdRphlJQHaK9?rs=1&pid=ImgDetMain&o=7&rm=3",
+      imdb: 8.7
+    },
+    {
+      id: 8,
+      nome: "Spider-Man",
+      imagem: "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg",
+      imdb: 7.4
+    },
+    {
+      id: 9,
+      nome: "White Chicks",
+      imagem: "https://tse4.mm.bing.net/th/id/OIP.3mphRIzwWiNg0c08slGQHQHaLH?rs=1&pid=ImgDetMain&o=7&rm=3",
+      imdb: 5.8
+    }
+  ];
+  
+  export default DATA;
